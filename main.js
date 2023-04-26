a36419df59c6105a06a7d64aee13793c8816931c
Alemax75 <alemax75962@gmail.com>
2023-04-26 15:27:34 +0300

    Доработка всплывающих окон

--- main.js
+++ main.js
@@ -7,7 +7,17 @@ let navClose = document.getElementById('nav-close')
 let loginPopup = document.getElementById('login-popup')
 let loginBtn = document.getElementById('login-btn')
 let login1Btn = document.getElementById('login-btn1')
+let login2Btn = document.getElementById('login-btn2')
 let loginClose = document.getElementById('login-close')
+
+let authBtn = document.getElementById('auth-btn')
+let authPopup = document.getElementById('auth-popup')
+let authClose = document.getElementById('auth-close')
+let authBack = document.getElementById('auth-back')
+
+let clothBtn = document.getElementById('cloth-btn')
+let clothPopup = document.getElementById('cloth-popup')
+let clothBack = document.getElementById('cloth-back')
 
 let searchPopup = document.getElementById('search-popup')
 let searchBtn = document.getElementById('search-btn')
@@ -17,6 +27,8 @@ navBtn.addEventListener('click', () => {
     navPopup.classList.add('popup_active')
     searchPopup.classList.remove('popup_active')
     loginPopup.classList.remove('popup_active')
+    authPopup.classList.remove('popup_active')
+    clothPopup.classList.remove('popup_active')
 })
 
 navClose.addEventListener('click', () => {
@@ -27,6 +39,8 @@ searchBtn.addEventListener('click', () => {
     searchPopup.classList.add('popup_active')
     navPopup.classList.remove('popup_active')
     loginPopup.classList.remove('popup_active')
+    authPopup.classList.remove('popup_active')
+    clothPopup.classList.remove('popup_active')
 })
 
 searchClose.addEventListener('click', () => {
@@ -37,6 +51,8 @@ loginBtn.addEventListener('click', () => {
     loginPopup.classList.add('popup_active')
     navPopup.classList.remove('popup_active')
     searchPopup.classList.remove('popup_active')
+    authPopup.classList.remove('popup_active')
+    clothPopup.classList.remove('popup_active')
 })
 
 loginClose.addEventListener('click', () => {
@@ -47,8 +63,61 @@ login1Btn.addEventListener('click', () => {
     loginPopup.classList.add('popup_active')
     navPopup.classList.remove('popup_active')
     searchPopup.classList.remove('popup_active')
+    authPopup.classList.remove('popup_active')
+    clothPopup.classList.remove('popup_active')
 })
+
+login2Btn.addEventListener('click', () => {
+    
+    loginPopup.classList.add('popup_active')
+    navPopup.classList.remove('popup_active')
+    searchPopup.classList.remove('popup_active')
+    authPopup.classList.remove('popup_active')
+    clothPopup.classList.remove('popup_active')
+})
+
 
 loginClose.addEventListener('click', () => {
     loginPopup.classList.remove('popup_active')
+})
+
+authBtn.addEventListener('click', () => {
+    authPopup.classList.add('popup_active')
+    loginPopup.classList.remove('popup_active')
+    navPopup.classList.remove('popup_active')
+    searchPopup.classList.remove('popup_active')
+    clothPopup.classList.remove('popup_active')
+})
+
+authClose.addEventListener('click', () => {
+    authPopup.classList.remove('popup_active')
+    loginPopup.classList.remove('popup_active')
+    navPopup.classList.remove('popup_active')
+    searchPopup.classList.remove('popup_active')
+    clothPopup.classList.remove('popup_active')
+})
+
+
+clothBack.addEventListener('click', () => {
+    navPopup.classList.add('popup_active')
+    searchPopup.classList.remove('popup_active')
+    loginPopup.classList.remove('popup_active')
+    authPopup.classList.remove('popup_active')
+    clothPopup.classList.remove('popup_active')
+})
+
+clothBtn.addEventListener('click', () => {
+    clothPopup.classList.add('popup_active')
+    searchPopup.classList.remove('popup_active')
+    loginPopup.classList.remove('popup_active')
+    authPopup.classList.remove('popup_active')
+    navPopup.classList.remove('popup_active')
+})
+
+authBack.addEventListener('click', () => {
+    navPopup.classList.remove('popup_active')
+    searchPopup.classList.remove('popup_active')
+    loginPopup.classList.add('popup_active')
+    authPopup.classList.remove('popup_active')
+    clothPopup.classList.remove('popup_active')
 })
